5d79ff35040628992128d5f313f91f516f907c53
zaidprd <160503080+zaidprd@users.noreply.github.com>
2026-05-26 00:17:39 +0700

    Update konfigurasi next.config untuk Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export", // <--- Hapus tanda // agar menjadi web statis
   trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // Bagus, agar gambar dari Sanity CDN bisa langsung tampil
   },
 };
 export default nextConfig;
