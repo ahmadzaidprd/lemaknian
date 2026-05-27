719eed423202acc4d71535627d7125eddafc59c6
zaidprd <160503080+zaidprd@users.noreply.github.com>
2026-05-27 21:51:53 +0700

    fix: next.config headers regex

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,8 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*\\.(jpg|jpeg|png|webp|avif|svg|ico|woff|woff2|ttf|otf|css|js))",
+        // Ganti capturing group → non-capturing group (?:...)
+        source: "/(:path*\.(?:jpg|jpeg|png|webp|avif|svg|ico|woff|woff2|ttf|otf|css|js))",
         headers: [
           { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
         ],
